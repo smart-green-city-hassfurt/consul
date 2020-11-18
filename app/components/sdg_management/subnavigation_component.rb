551b996ec2017ef4c5992d8b41258adc233f0574
Senén Rodero Rodríguez <senenrodero@gmail.com>
2020-11-18 22:08:27 +0100

    Add subnavigation component to targets and goals
    Add a new SDG component to make easier to create subnavigation menus.
    
    Co-Authored-By: Javi Martín <35156+javierm@users.noreply.github.com>

---
+++ app/components/sdg_management/subnavigation_component.rb
@@ -0,0 +1,26 @@
+class SDGManagement::SubnavigationComponent < ApplicationComponent
+  attr_reader :current
+
+  def initialize(current:)
+    @current = current
+  end
+
+  private
+
+    def sections
+      %i[goals targets]
+    end
+
+    def link_to_section(section)
+      link_to "SDG::#{section.to_s.classify}".constantize.model_name.human(count: 2).titleize,
+              path_for(section),
+              "aria-selected": section == current
+    end
+
+    def path_for(section)
+      {
+        controller: "sdg_management/#{section}",
+        action: :index
+      }
+    end
+end

1101338e188720a2d8224790132f18e1d380a540
Javi Martín <javim@elretirao.net>
2020-11-18 19:35:17 +0100

    Add relations between relatable models and SDGs
    Note we cannot directly get all related SDGs through SQL because they're
    spread through different tables.

--- app/models/concerns/sdg/relatable.rb
+++ app/models/concerns/sdg/relatable.rb
@@ -3,5 +3,16 @@ module SDG::Relatable
 
   included do
     has_many :sdg_relations, as: :relatable, dependent: :destroy, class_name: "SDG::Relation"
+
+    %w[Goal Target].each do |sdg_type|
+      has_many :"sdg_#{sdg_type.tableize}",
+               through: :sdg_relations,
+               source: :related_sdg,
+               source_type: "SDG::#{sdg_type}"
+    end
+  end
+
+  def related_sdgs
+    sdg_relations.map(&:related_sdg)
   end
 end

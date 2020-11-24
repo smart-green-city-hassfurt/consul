eb3947106b40ad4049f7d6940f66f49c85e973d2
Senén Rodero Rodríguez <senenrodero@gmail.com>
2020-11-24 18:05:44 +0100

    Allow component to receive options for page header

--- app/components/sdg_management/header.rb
+++ app/components/sdg_management/header.rb
@@ -1,11 +1,13 @@
 module SDGManagement::Header
   extend ActiveSupport::Concern
 
-  def header
+  def header(options: {})
     provide(:title) do
       "#{t("sdg_management.header.title")} - #{title}"
     end
 
-    tag.h2 title
+    tag.h2 options do
+      title
+    end
   end
 end

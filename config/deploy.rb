abb0af1b025798c539684f60bd035c9daceeb656
Diethard Sahlender <diethard.sahlender@mac-pro.sv-exchange.dom.stwhas.de>
2020-12-30 11:08:59 +0100

    Add sample text to homepage

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :application, "consul"
 set :deploy_to, deploysecret(:deploy_to)
 set :ssh_options, port: deploysecret(:ssh_port)
 
-set :repo_url, "https://github.com/smart-green-city-hassfurt/consul.git"
+set :repo_url, "https://github.com/smart-green-city-hassfurt/consul"
 
 set :revision, `git rev-parse --short #{fetch(:branch)}`.strip
 
